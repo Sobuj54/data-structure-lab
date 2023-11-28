62a2eee420fb282d1b5b5b8dfc11dab089c68ed8
soubj <sobujahmed124@gmail.com>
2023-11-29 02:28:07 +0600

    queue operations

---
+++ 15_queue-operations.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct queue
+{
+    int size;
+    int f;
+    int r;
+    int *arr;
+};
+
+int isFull(struct queue *q)
+{
+    if (q->r == q->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isEmpty(struct queue *q)
+{
+    if (q->r == q->f)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void enqueue(struct queue *q, int val)
+{
+    if (isFull(q))
+    {
+        printf("full");
+    }
+    else
+    {
+        q->r++;
+        q->arr[q->r] = val;
+    }
+}
+
+int dequeue(struct queue *q)
+{
+    int a = -1;
+    if (isEmpty(q))
+    {
+        printf("empty");
+    }
+    else
+    {
+        q->f++;
+        a = q->arr[q->f];
+    }
+    return a;
+}
+
+int main()
+{
+    struct queue q;
+    q.size = 10;
+    q.f = q.r = -1;
+    q.arr = (int *)malloc(q.size * sizeof(int));
+
+    enqueue(&q, 15);
+    enqueue(&q, 10);
+    enqueue(&q, 18);
+    printf("%d dequeued", dequeue(&q));
+    return 0;
+}
